597aa07dab8cf567943d7ddf4f35c746e5a6a4af
AggelosAlek <91098216+AggelosAlek@users.noreply.github.com>
2022-06-04 01:37:55 +0300

    Add files via upload

---
+++ New folder/readme.txt
@@ -0,0 +1,1 @@
+font-family: Helvetica, Sans-Serif;
---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function shownav() {
+    document.getElementById("header-nav").style.display = "block";
+    document.getElementById("x").style.display = "block";
+}
+
+function hidenav() {
+    document.getElementById("header-nav").style.display = "none";
+    document.getElementById("x").style.display = "none";
+}
